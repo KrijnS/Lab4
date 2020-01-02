39524f0acee7f647c28bb7bd6c3d0add17574e9e
Krijn van der Steen <krijnvdsteen@live.nl>
2020-01-02 17:30:26 +0100

    Inverted y-axis

--- ShapeDrawing/TEXGenerator.cs
+++ ShapeDrawing/TEXGenerator.cs
@@ -18,24 +18,17 @@ namespace ShapeDrawing
 
         public override void CreateCircle(int x, int y, int size, Color color)
         {
-            double correctedX = x / 50f;
-            double correctedY = y / 50f;
             double correctedR = size / 100f;
           
-            string output = "\\definecolor{myColor}{RGB}{" +color.R + ',' + color.G + ',' + color.B + "}\n\\draw[myColor] (" + correctedX.ToString(new CultureInfo("en-US")) 
-                + ',' + correctedY.ToString(new CultureInfo("en-US")) + ") circle (" + correctedR.ToString(new CultureInfo("en-US")) + "cm);";
+            string output = "\\definecolor{myColor}{RGB}{" +color.R + ',' + color.G + ',' + color.B + "}\n\\draw[myColor] (" + ConvertX(x)
+                + ',' + ConvertY(y) + ") circle (" + correctedR.ToString(new CultureInfo("en-US")) + "cm);";
             texOutput.Add(output);
         }
 
         public override void CreateRectangle(int x, int y, int width, int height, Color color)
-        {
-            double correctedX = x / 50f;
-            double correctedY = y / 50f;
-            double x2 = (x + width) / 50f;
-            double y2 = (y + height) / 50f;
-    
-            string output = "\\definecolor{myColor}{RGB}{" + color.R + ',' + color.G + ',' + color.B + "}\n\\draw[myColor] (" + correctedX.ToString(new CultureInfo("en-US")) 
-                + ',' + correctedY.ToString(new CultureInfo("en-US")) + ") rectangle (" + x2.ToString(new CultureInfo("en-US")) + ',' + y2.ToString(new CultureInfo("en-US")) + ");";
+        {        
+            string output = "\\definecolor{myColor}{RGB}{" + color.R + ',' + color.G + ',' + color.B + "}\n\\draw[myColor] (" + ConvertX(x)
+                + ',' + ConvertY(y) + ") rectangle (" + ConvertX(x+width) + ',' + ConvertY(y+height) + ");";
             texOutput.Add(output);
         }
 
@@ -43,19 +36,32 @@ namespace ShapeDrawing
         { 
             Star star = new Star(null, 0, 0, 0, 0, color);
             Point[] points = star.CalculateStarPoints(x, y, width, height, star.numPoints);
-            float correctedX1 = points[0].X / 50f;
-            float correctedY1 = points[0].Y / 50f;
-            string output = "\\definecolor{myColor}{RGB}{" + color.R + ',' + color.G + ',' + color.B + "}\n\\draw[myColor] (" + correctedX1.ToString(new CultureInfo("en-US")) + ',' + correctedY1.ToString(new CultureInfo("en-US")) + ')';
+            string output = "\\definecolor{myColor}{RGB}{" + color.R + ',' + color.G + ',' + color.B + "}\n\\draw[myColor] (" + ConvertX(points[0].X) + ',' + ConvertY(points[0].Y) + ')';
             for (int i = 1; i < points.Length; i++)
             {
-                float correctedX = points[i].X / 50f;
-                float correctedY = points[i].Y / 50f;
-                output += " -- (" + correctedX.ToString(new CultureInfo("en-US")) + "," + correctedY.ToString(new CultureInfo("en-US")) + ')';
+                output += " -- (" + ConvertX(points[i].X) + "," + ConvertY(points[i].Y) + ')';
             }
 
-            output += " -- (" + correctedX1.ToString(new CultureInfo("en-US")) + ',' + correctedY1.ToString(new CultureInfo("en-US")) + ");";
+            output += " -- (" + ConvertX(points[0].X) + ',' + ConvertY(points[0].Y) + ");";
         
             texOutput.Add(output);
+        }
+
+        //Method that firstly divides y and then inverts the y with a given number to fit the LaTeX format. Afterwards it converts to a decimal dot notation
+        public string ConvertY(int y)
+        {
+            //divide to fit pixels to centimeters
+            float divide = y / 50f;
+            //20 is the number that is about the height of a LaTeX page
+            float result = 20 - divide;
+            return result.ToString(new CultureInfo("en-US"));
+        }
+
+        //Method that divides x to make pixels to centimeters. Afterwards it converts to a decimal dot notation
+        public string ConvertX(int x)
+        {
+            float result = x / 50f;
+            return result.ToString(new CultureInfo("en-US"));
         }
     }
 }
