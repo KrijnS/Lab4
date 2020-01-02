3dfe47df2cccace16d9448fa90faff60fbaeaec9
Krijn van der Steen <krijnvdsteen@live.nl>
2020-01-02 13:46:50 +0100

    Rectangle implementation, omitted unnecessary code

--- ShapeDrawing/SVGGenerator.cs
+++ ShapeDrawing/SVGGenerator.cs
@@ -17,32 +17,25 @@ namespace ShapeDrawing
 
         public override void CreateCircle(int x, int y, int size, Color color)
         {
-            Pen pen = new Pen(color);
             int r = size / 2;
             int cx = x + r;
             int cy = y + r;
             string output = "   <circle cx=\u0022" + cx + "\u0022 cy=\u0022" + cy + "\u0022 r=\u0022" + r +
-                "\u0022 stroke=\u0022rgb(" + pen.Color.R + ',' + pen.Color.G + ',' + pen.Color.B + ")\u0022 fill=\u0022none\u0022 />";
+                "\u0022 stroke=\u0022rgb(" + color.R + ',' + color.G + ',' + color.B + ")\u0022 fill=\u0022none\u0022 />";
 
             svgOutput.Add(output);
         }
 
         public override void CreateRectangle(int x, int y, int width, int height, Color color)
         {
-            Pen pen = new Pen(color);
-            int x1 = x;
-            int x2 = x1 + width;
-            int y1 = y;
-            int y2 = y1 + height;
-            string output = "   <polyline points=\u0022" + x1 + ',' + y1 + ' ' + x2 + ',' + y1 + ' ' + x2 + ',' + y2 + ' ' + x1 + ',' + y2 + ' ' + x1 + ',' + y1 +
-            "\u0022 stroke=\u0022rgb(" + pen.Color.R + ',' + pen.Color.G + ',' + pen.Color.B + ")\u0022 fill=\u0022none\u0022/>";
+            string output = "   <rect x=\u0022" + x + "\u0022 y=\u0022 " + y + "\u0022 width=\u0022" + width + "\u0022 height=\u0022" + height + 
+            "\u0022 stroke=\u0022rgb(" + color.R + ',' + color.G + ',' + color.B + ")\u0022 fill=\u0022none\u0022/>";
 
             svgOutput.Add(output);
         }
 
         public override void CreateStar(int x, int y, int width, int height, Color color)
-        {
-            Pen pen = new Pen(color);
+        { 
             Star star = new Star(null, 0, 0, 0, 0, color);
             Point[] points = star.CalculateStarPoints(x, y, width, height, star.numPoints);
             string output = "   <polyline points=\u0022" + points[0].X + ',' + points[0].Y;
@@ -51,7 +44,7 @@ namespace ShapeDrawing
                 output += " " + points[i].X + "," + points[i].Y;
             }
 
-            output += " " + points[0].X + "," + points[0].Y + "\u0022 stroke=\u0022rgb(" + pen.Color.R + ',' + pen.Color.G + ',' + pen.Color.B + ")\u0022 fill=\u0022none\u0022/>";
+            output += " " + points[0].X + "," + points[0].Y + "\u0022 stroke=\u0022rgb(" + color.R + ',' + color.G + ',' + color.B + ")\u0022 fill=\u0022none\u0022/>";
 
             svgOutput.Add(output);
         }
