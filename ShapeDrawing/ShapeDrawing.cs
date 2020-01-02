20146300268ecb8f3a773acb8fcbb9ca8ea75b7a
Krijn van der Steen <krijnvdsteen@live.nl>
2020-01-02 15:28:10 +0100

    Rectangle and circle TEX implementation

--- ShapeDrawing/ShapeDrawing.cs
+++ ShapeDrawing/ShapeDrawing.cs
@@ -62,33 +62,71 @@ public class ShapeDrawingForm : Form
 		Stream stream;
 		SaveFileDialog saveFileDialog = new SaveFileDialog();
 
-		saveFileDialog.Filter = "SVG files|*.svg";
+		saveFileDialog.Filter = "SVG files|*.svg|LaTeX files|*.tex";
 		saveFileDialog.RestoreDirectory = true;
-		
+
 		if(saveFileDialog.ShowDialog() == DialogResult.OK)
 		{
 			if((stream = saveFileDialog.OpenFile()) != null)
 			{
-                string intro = "<?xml version=\u00221.0\u0022 standalone=\u0022no\u0022?>\n<!DOCTYPE svg PUBLIC \u0022-//W3C//DTD SVG 1.1//EN\u0022"
-                    + "\n   \u0022http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" + "\u0022>\n<svg xmlns=\u0022http://www.w3.org/2000/svg" + "\u0022 version=\u00221.1\u0022>";
-                using (StreamWriter writer = new StreamWriter(stream))
+                string extension = Path.GetExtension(saveFileDialog.FileName);
+
+                switch (extension.ToLower())
                 {
-                    writer.WriteLine(intro);
-                    SVGGenerator svgGenerator = new SVGGenerator();
-                    foreach(Shape shape in shapes)
-                    {
-                        shape.SetBridge(svgGenerator);
-                        shape.Create();                        
-                    }                 
-                    foreach(string output in svgGenerator.svgOutput)
-                    {
-                        writer.WriteLine(output);
-                    }
-                    writer.WriteLine("</svg>");
-                }				
+                    case ".svg":
+                        WriteSVG(stream);
+                        break;
+                    case ".tex":
+                        WriteTEX(stream);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(extension);
+                }
 			}
 		}
 	}
+
+    private void WriteSVG(Stream stream)
+    {
+        string intro = "<?xml version=\u00221.0\u0022 standalone=\u0022no\u0022?>\n<!DOCTYPE svg PUBLIC \u0022-//W3C//DTD SVG 1.1//EN\u0022"
+                    + "\n   \u0022http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" + "\u0022>\n<svg xmlns=\u0022http://www.w3.org/2000/svg" + "\u0022 version=\u00221.1\u0022>";
+        using (StreamWriter writer = new StreamWriter(stream))
+        {
+            writer.WriteLine(intro);
+            SVGGenerator svgGenerator = new SVGGenerator();
+            foreach (Shape shape in shapes)
+            {
+                shape.SetBridge(svgGenerator);
+                shape.Create();
+            }
+            foreach (string output in svgGenerator.svgOutput)
+            {
+                writer.WriteLine(output);
+            }
+            writer.WriteLine("</svg>");
+        }
+    }
+
+    private void WriteTEX(Stream stream)
+    {
+        string intro = "\\documentclass{article}\n\\usepackage{tikz}\n\\usepackage{xcolor}\n\\begin{document}\n\\begin{tikzpicture}";
+        using (StreamWriter writer = new StreamWriter(stream))
+        {
+            writer.WriteLine(intro);
+            TEXGenerator texGenerator = new TEXGenerator();
+            foreach (Shape shape in shapes)
+            {
+                shape.SetBridge(texGenerator);
+                shape.Create();
+            }
+            foreach (string output in texGenerator.texOutput)
+            {
+                writer.WriteLine(output);
+            }
+            writer.WriteLine("\\end{tikzpicture}\n\\end{document}");
+        }
+
+    }
 
     private void OnPaint(object sender, PaintEventArgs e)
 	{
--- ShapeDrawing/TEXGenerator.cs
+++ ShapeDrawing/TEXGenerator.cs
@@ -16,28 +16,20 @@ namespace ShapeDrawing
         }
 
         public override void CreateCircle(int x, int y, int size, Color color)
-        {
-            Pen pen = new Pen(color);
-            //int r = size / 2;
-            //int cx = x + r;
-            //int cy = y + r;
-            //string output = "   <circle cx=\u0022" + cx + "\u0022 cy=\u0022" + cy + "\u0022 r=\u0022" + r +
-            //    "\u0022 stroke-width=\u00221\u0022 fill=\u0022none\u0022 stroke=\u0022" + pen + "\u0022 />";
-            //texOutput.Add(output);
+        { 
+            int r = size / 2;
+          
+            string output = "\\definecolor{myColor}{RGB}{" +color.R + ',' + color.G + ',' + color.B + "}\n\\draw[myColor] (" + x + ',' + y + ") circle (" + r + "cm);";
+            texOutput.Add(output);
         }
 
         public override void CreateRectangle(int x, int y, int width, int height, Color color)
         {
-            Pen pen = new Pen(color);
-
-            //int x1 = x;
-            //int x2 = x1 + width;
-            //int y1 = y;
-            //int y2 = y1 + height;
-            //string output = "   <polyline points=\u0022" + x1 + ',' + y1 + ' ' + x2 + ',' + y1 + ' ' + x2 + ',' + y2 + ' ' + x1 + ',' + y2 + ' ' + x1 + ',' + y1 +
-            //"\u0022 style=\u0022fill:none;stroke:\u0022" + pen + "\u0022;stroke-width:1\u0022 />";
-            
-            //texOutput.Add(output);
+            int x2 = x + width;
+            int y2 = y + height;
+    
+            string output = "\\definecolor{myColor}{RGB}{" + color.R + ',' + color.G + ',' + color.B + "}\n\\draw (" + x + ',' + y + ") rectangle (" + x2 + ',' + y2 + ");";
+            texOutput.Add(output);
         }
 
         public override void CreateStar(int x, int y, int width, int height, Color color)
